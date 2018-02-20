255bb72275dfea93f6775d0a49ab87b0a68ae156
Danyel Murray <danyelmurray1987@gmail.com>
2018-02-20 15:38:37 -0800

    fix syntax errors in interface.

--- js/betterDoctor-interface.js
+++ js/betterDoctor-interface.js
@@ -1,7 +1,7 @@
 import { promise } from './../js/betterDoctor.js';
 
 $(document).ready(function () {
-$('#search').submit(function (event) {
+	$('#search').submit(function (event) {
 		event.preventDefault();
 		let name = $('#name').val();
 		let symptom = $('#symptom').val();
@@ -15,19 +15,18 @@ $('#search').submit(function (event) {
 					$('#result').append('<h2>' + body.data[i].profile.first_name + ' ' + body.data[i].profile.last_name + '</h2>');
 
 
-				);
+				}
 
 				if (body.data.length < 1) {
 					$('#result').text("No doctors found.");
+				} else {
+					$('#result').append('<li>' + body.data[i].practices[0].website + '</li>');
 				}
-			} else {
-				$('#result').append('<li>' + body.data[i].practices[0].website + '</li>');
-			}
-		}
-	},
-	function (error) {
-		$('#result').text("There was an error processing your request. Please try again.");
 
+			},
+			function (error) {
+				$('#result').text("There was an error processing your request. Please try again.");
+
+			});
 	});
-});
 });
