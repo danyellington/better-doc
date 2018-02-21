8d0697a1ef04fc7d0244d63123871ecd3e4a841c
Danyel Murray <danyelmurray1987@gmail.com>
2018-02-20 16:05:47 -0800

    change interface to incorporate if statements and working on loop.

--- js/betterDoctor-interface.js
+++ js/betterDoctor-interface.js
@@ -8,25 +8,20 @@ $(document).ready(function () {
 
 		let newSearch = promise(name, symptom);
 		$('#result').empty();
-		// debugger;
+		debugger;
 		newSearch.then(function (response) {
 				let body = JSON.parse(response);
-				for (let i = 0; i < body.data.length; i++) {
-					$('#result').append('<h2>' + body.data[i].profile.first_name + ' ' + body.data[i].profile.last_name + '</h2>');
-
-
-				}
-
 				if (body.data.length < 1) {
 					$('#result').text("No doctors found.");
 				} else {
-					$('#result').append('<li>' + body.data[i].practices[0].website + '</li>');
+					for (let i = 0; i < body.data.length; i++) {
+						$('#result').append('<p>' + body.data[i].profile.first_name + ' ' + body.data[i].profile.last_name + '</p>');
+					}
 				}
-
 			},
 			function (error) {
 				$('#result').text("There was an error processing your request. Please try again.");
-
-			});
+			}
+		);
 	});
 });
--- js/betterDoctor.js
+++ js/betterDoctor.js
@@ -2,7 +2,11 @@ import apiKey from './../.env';
 export let promise = function (name, symptom) {
 	return new Promise(function (resolve, reject) {
 		let request = new XMLHttpRequest();
-		let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`;
+		let apiKey = api_key;
+		let location = "37.773%2C-122.413%2C100";
+		let user_location = "37.773%2C-122.413";
+		let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=${location}&user_location=${user_location}&skip=0&user_key=${api_key}`;
+
 		request.onload = function () {
 			if (this.status === 200) {
 				resolve(request.response);
