36440d46afa07a8806c7fbafa16356f8d51b9b7d
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-22 09:39:53 -0800

    fix api call and finish interface to retrieve doctor profiles.

--- js/betterDoctor-interface.js
+++ js/betterDoctor-interface.js
@@ -8,14 +8,38 @@ $(document).ready(function () {
 
 		let newSearch = promise(name, symptom);
 		$('#result').empty();
-		// debugger;
 		newSearch.then(function (response) {
 				let body = JSON.parse(response);
 				if (body.data.length < 1) {
 					$('#result').text("No doctors found.");
 				} else {
 					for (let i = 0; i < body.data.length; i++) {
-						$('#result').append('<p>' + body.data[i].profile.first_name + ' ' + body.data[i].profile.last_name + '</p>');
+						let first_name = body.data[i].profile.first_name;
+						let last_name = body.data[i].profile.last_name;
+						$('#result').append(`<br><br><br><strong>${last_name}, ${first_name}</strong>`);
+
+						for (let j = 0; j < body.data[i].practices.length; j++) {
+							let streetAddress = body.data[i].practices[j].visit_address.street;
+							let address_city = body.data[i].practices[j].visit_address.city;
+							let address_state_long = body.data[i].practices[j].visit_address.state_long;
+							let address_zip = body.data[i].practices[j].visit_address.zip;
+
+							$("#result").append(`<br>${streetAddress}, <br>${address_city}, ${address_state_long} ${address_zip}`);
+
+							let patients = body.data[i].practices[j].accepts_new_patients;
+							let website = body.data[i].practices[j].website;
+							if (website === undefined) {
+								$("#result").append(`<br>No website available.`);
+							} else {
+								$("#result").append(`<br><a href="${website}">${website}</a>`);
+							}
+							for (let k = 0; k < body.data[i].practices[j].phones.length; k++) {
+								let type = body.data[i].practices[j].phones[k].type;
+								let number = body.data[i].practices[j].phones[k].number;
+								$('#result').append(`<br>${type}: ${number}`)
+
+							}
+						}
 					}
 				}
 			},
--- js/betterDoctor.js
+++ js/betterDoctor.js
@@ -1,10 +1,9 @@
-import apiKey from './../.env';
+const apiKey = require('./../.env').apiKey;
 
 export let promise = function (name, symptom) {
 	return new Promise(function (resolve, reject) {
 		let request = new XMLHttpRequest();
-		// let api_key = apiKey;
-		let location = "37.773%2C-122.413%2C100";
+		let location = "or-portland";
 		let user_location = "37.773%2C-122.413";
 		let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=${location}&user_location=${user_location}&skip=0&limit=10&user_key=${apiKey}`;
 		request.onload = function () {
